2b25504c0124190fd33caaee7cf8b13cd885d53e
Mohamed SALIFOU DJITAO <sdjitao@MacBook-Pro-de-Mohamed.local>
2019-12-02 23:50:39 +0100

    mise2

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Controller;
-
+use App\Entity\Theses;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,8 +12,32 @@ class HomeController extends AbstractController
      */
     public function index()
     {
+        $entityManager = $this ->getDoctrine() ->getManager();
+        $ThesesRepository = $entityManager ->getRepository(Theses :: class);
+        $these = $ThesesRepository ->findAll();
+
+        if (empty($these)) {
+            $these0 = new Theses();
+            $these0 ->setTitle('rotage mobile');
+            $these0 ->setPhrase('Méthodes approximative au problème de routage de véhicule pour une gestion de flotte de drones');
+            $these0 ->setContact('univ1@gmail.com');
+            $entityManager ->persist($these0);
+
+            $these1 = new Theses();
+            $these1 ->setTitle('systeme embarqué');
+            $these1 ->setPhrase('Système embarqué de fusion multi-capteurs pour la détection et le suivi obstacles statiques et dynamiques');
+            $these1 ->setContact('univ2@gmail.com');
+            $entityManager ->persist($these1);
+
+            $these2 = new Theses();
+            $these2 ->setTitle('chaine logistique');
+            $these2 ->setPhrase('Modèles mathématiques et heuristiques pour l’optimisation des chaînes logistiques portuaire du vrac');
+            $these2 ->setContact('ecolepolytechnique@gmail.com');
+            $entityManager ->persist($these2);
+            $entityManager ->flush();
+        }
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'these' => $ThesesRepository->findAll(),
         ]);
     }
 }
---
+++ src/Entity/Theses.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ThesesRepository")
+ */
+class Theses
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $phrase;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $contact;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getPhrase(): ?string
+    {
+        return $this->phrase;
+    }
+
+    public function setPhrase(string $phrase): self
+    {
+        $this->phrase = $phrase;
+
+        return $this;
+    }
+
+    public function getContact(): ?string
+    {
+        return $this->contact;
+    }
+
+    public function setContact(string $contact): self
+    {
+        $this->contact = $contact;
+
+        return $this;
+    }
+}
